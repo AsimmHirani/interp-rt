5112f2bd6ca310c306f82e001aff271fc96bc17c
ridefox-ahirani <asimm.hirani@gmail.com>
2021-08-25 22:53:40 -0400

    Added skeleton code and binary sort

---
+++ include/interp-rt.h
@@ -0,0 +1,20 @@
+#ifndef INTERP_RT_INTERP_RT_H
+#define INTERP_RT_INTERP_RT_H
+
+// If 1, we will extrapolate outside dataset
+#define INTERP_RT_EXTRAPOLATE (0)
+#define INTERP_RT_FRAC_BIT ((unsigned int) 16)
+#define INTERP_RT_ERR   (-1)
+
+typedef unsigned int INTERP_RT_dt; /* Default Datatype */
+
+/* Lookup table definition */
+typedef struct INTERP_RT_lut {
+    INTERP_RT_dt* x; // must be sorted, x indices
+    INTERP_RT_dt* y; // must be sorted, y indices
+    unsigned int len; // number of elements
+} INTERP_RT_lut_t;
+
+INTERP_RT_dt INTERP_RT_interpolate1d(INTERP_RT_lut_t const * lut, INTERP_RT_dt x0);
+
+#endif //INTERP_RT_INTERP_RT_H
--- library.h
+++
@@ -1,6 +0,0 @@
-#ifndef INTERP_RT_LIBRARY_H
-#define INTERP_RT_LIBRARY_H
-
-void hello(void);
-
-#endif //INTERP_RT_LIBRARY_H
